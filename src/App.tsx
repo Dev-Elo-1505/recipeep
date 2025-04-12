3bfdc5077688d4d9a527eb7d508e3481112e78ee
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-04-12 15:54:53 +0100

    setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+// I basically want people to view recipes and search for recipes.
+// Milestones:
+// 1. Route to the homepage so i can route to the details page
+// 2. build nav and hero section [without searchbar]
+// 3. fetch and display recipes
+// 4. build recipe card
+// 5. route to the recipe details page
+// 6. build recipe details page
+// 7. build search bar
+// 8. build search results page
 
-import './App.css'
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 function App() {
- 
-
   return (
-   <div className='text-orange-500'>recipeep</div>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
